7d7ae052a144c8646367bc0d7bc59e018890a175
JadavPrince0102 <jadavprince232002@gmail.com>
2025-05-03 17:34:22 +0530

    Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 function App() {
+
+  const [products, setProducts] = useState([])
+
+  const fetchData = async () => {
+      const res = await fetch("https://dummyjson.com/products?limit=500")
+      const data = await res.json()
+      setProducts(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+ 
+
   return (
     <>
       <div className="main-div">
